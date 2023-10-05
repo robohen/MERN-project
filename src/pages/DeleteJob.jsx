4b953b190c23f16c5f2ec2f4c1ce53030f1f043b
robohen <88636103+robohen@users.noreply.github.com>
2023-10-04 19:36:52 -0700

    pages for some of the set up created will continue more tomorrow

---
+++ src/pages/DeleteJob.jsx
@@ -0,0 +1,5 @@
+const DeleteJob = () => {
+  return <h1>Delete Job Page</h1>;
+};
+
+export default DeleteJob;
---
+++ src/pages/Error.jsx
@@ -0,0 +1,12 @@
+import { Link, useRouteError } from "react-router-dom";
+const Error = () => {
+  const error = useRouteError();
+  return (
+    <div>
+      <h1>Error Page</h1>
+      <Link to="/">Back Home</Link>
+    </div>
+  );
+};
+
+export default Error;
