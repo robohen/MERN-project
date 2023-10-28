9d59445a17716ded9d263ad090f8ebf60d827405
robohen <88636103+robohen@users.noreply.github.com>
2023-10-28 09:29:29 -0700

    new vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+ 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      '/uploads': {
+        target: 'http://localhost:5173/uploads',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/uploads/, ''),
+      },
       '/api': {
         target: 'http://localhost:5173/api',
         changeOrigin: true,
